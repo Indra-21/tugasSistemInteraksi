235620dc4d2cb1d6e62ffc26b3ed55d9557bbb67
INDRA-SAPUTRA\Indra <indra.saputra@ist.id>
2022-09-13 15:42:19 +0700

    fixing deploy

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 import IconComp from './Components/IconComp';
 import NavbarComp from './Components/NavbarComp';
 import Home from './Pages/Home';
-import InstagramEmbed from "react-instagram-embed";
+
 
 function App() {
   return (
@@ -16,19 +16,7 @@ function App() {
           <Route path="/" element={<Home />} />
         </Routes>
       </BrowserRouter>
-      <InstagramEmbed
-            url='https://www.instagram.com/p/CiacMiaoL37/'
-            clientAccessToken='123|456'
-            maxWidth={630}
-            hideCaption={false}
-            containerTagName='div'
-            protocol=''
-            injectScript
-            onLoading={() => {}}
-            onSuccess={() => {}}
-            onAfterRender={() => {}}
-            onFailure={() => {}}
-          />
+      
     </div>
   );
 }
